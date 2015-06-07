978871918178c44caeaf0c7fdb87f6ea7d5817a0
Thomas Horrobin <thomasroberthorrobin@gmail.com>
2015-06-07 22:59:48 +1200

    Fixed authication problem

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
 
   #root 'owners#index'
 
-  root to: 'owners#index'
+  root 'owners#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
