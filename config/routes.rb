11d97461987c99c1448ad2dee9e223ed71b297ca
Thomas Horrobin <thomasroberthorrobin@gmail.com>
2015-06-07 16:49:05 +1200

    added devise to the owner class

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :owners
   resources :regos
   resources :breeds
   resources :dogs
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150607035933) do
+ActiveRecord::Schema.define(version: 20150607044322) do
 
   create_table "breeds", force: :cascade do |t|
     t.string   "BreedName",  limit: 255
@@ -32,11 +32,24 @@ ActiveRecord::Schema.define(version: 20150607035933) do
   add_index "dogs", ["owner_id"], name: "index_dogs_on_owner_id", using: :btree
 
   create_table "owners", force: :cascade do |t|
-    t.string   "Name",          limit: 255
-    t.string   "PostalAddress", limit: 255
-    t.datetime "created_at",                null: false
-    t.datetime "updated_at",                null: false
+    t.string   "Name",                   limit: 255
+    t.string   "PostalAddress",          limit: 255
+    t.datetime "created_at",                                      null: false
+    t.datetime "updated_at",                                      null: false
+    t.string   "email",                  limit: 255, default: "", null: false
+    t.string   "encrypted_password",     limit: 255, default: "", null: false
+    t.string   "reset_password_token",   limit: 255
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          limit: 4,   default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip",     limit: 255
+    t.string   "last_sign_in_ip",        limit: 255
   end
+
+  add_index "owners", ["email"], name: "index_owners_on_email", unique: true, using: :btree
+  add_index "owners", ["reset_password_token"], name: "index_owners_on_reset_password_token", unique: true, using: :btree
 
   create_table "regos", force: :cascade do |t|
     t.integer  "RegoLength", limit: 4
