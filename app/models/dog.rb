b4d038bd9d40f8c080c46a148f919b8b5789e5fe
Thomas Horrobin <thomasroberthorrobin@gmail.com>
2015-06-08 12:17:09 +1200

    added methods to work out if dog has been registered

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -2,4 +2,14 @@ class Dog < ActiveRecord::Base
 	has_many :regos
 	belongs_to :breed
 	belongs_to :owner
+
+	def is_registered?
+		registered = false
+		regos.each do |rego|
+			if rego.current_rego?
+				registered = true
+			end
+		end
+		return registered
+	end
 end
--- app/models/rego.rb
+++ app/models/rego.rb
@@ -1,3 +1,11 @@
 class Rego < ActiveRecord::Base
   belongs_to :dog
+
+  def current_rego?
+  	if Time.now > StartDate && Time.now < EndDate
+  		return true
+  	else
+  		return false
+  	end
+  end
 end
