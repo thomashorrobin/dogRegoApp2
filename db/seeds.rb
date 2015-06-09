7b3148c81ff2aa51c756b18ad73a1860bccb5ca8
Thomas Horrobin <thomasroberthorrobin@gmail.com>
2015-06-09 23:22:13 +1200

    added more data to the seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,11 @@
 
 Breed.create([{ BreedName: 'Poodle' },{ BreedName: 'Bulldog' },{ BreedName: 'Labrador Retriever' },{ BreedName: 'Golden Retriever' },{ BreedName: 'German Shepherd' },{ BreedName: 'Yorkshire Terrier' },{ BreedName: 'Beagle' },{ BreedName: 'Boxer' },{ BreedName: 'Rottweiler' },{ BreedName: 'Chihuahua' }])
 PricePlan.create([{ months: 3, price: 25 },{ months: 6, price: 65 },{ months: 12, price: 85 }])
+will = Owner.create( Name: "William Fussey", PostalAddress: "123 Fake St" )
+trev = Dog.create( Name: "Trev", owner: will, Birthday: Time.now, breed: Breed.first )
+barry = Dog.create( Name: "Barry", owner: will, Birthday: Time.now, breed: Breed.first )
+sarah = Owner.create( Name: "Sarah Duncan", PostalAddress: "77 Northland Rd" )
+louis = Dog.create( Name: "Louis", owner: sarah, Birthday: Time.now, breed: Breed.first )
+Rego.create( RegoLength: 3, StartDate: Time.now, EndDate: Time.now + 3 * 4 * 7 * 24 * 60 * 60, dog: trev, price_plan: PricePlan.first )
+Rego.create( RegoLength: 6, StartDate: Time.now, EndDate: Time.now + 6 * 4 * 7 * 24 * 60 * 60, dog: barry, price_plan: PricePlan.first )
+Rego.create( RegoLength: 12, StartDate: Time.now, EndDate: Time.now + 12 * 4 * 7 * 24 * 60 * 60, dog: louis, price_plan: PricePlan.first )
